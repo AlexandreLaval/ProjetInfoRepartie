395ae39a22e4d4cf9e09199bfcf0444d5818f72c
Herst <herstalex@gmail.com>
2022-01-01 11:37:14 +0100

    Init

---
+++ src/test/java/com/ProjIR/ProjetLavalThoral/ProjetLavalThoralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjIR.ProjetLavalThoral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetLavalThoralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
