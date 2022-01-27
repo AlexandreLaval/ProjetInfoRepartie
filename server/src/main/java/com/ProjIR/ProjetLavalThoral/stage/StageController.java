2f84f94ff3ccb96300c9d3edcec61324a59a4a2d
Herst <herstalex@gmail.com>
2022-01-27 19:51:57 +0100

    Ajout des methodes du controller stage / service stage

--- server/src/main/java/com/ProjIR/ProjetLavalThoral/stage/StageController.java
+++ server/src/main/java/com/ProjIR/ProjetLavalThoral/stage/StageController.java
@@ -1,10 +1,58 @@
 package com.ProjIR.ProjetLavalThoral.stage;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/stage")
 public class StageController {
+    private final StageService stageService;
 
+    @Autowired
+    public StageController(StageService stageService) {
+        this.stageService = stageService;
+    }
+
+    @GetMapping("/{numStage}")
+    public ResponseEntity<Stage> description(
+            @PathVariable(value = "numStage") Integer numStage) {
+        return new ResponseEntity<>(this.stageService.findStageByNumStage(numStage), HttpStatus.OK);
+    }
+
+    @GetMapping("/etudiant/{numEtudiant}")
+    public ResponseEntity<Stage> descriptionStageOfOneEtudiant(
+            @PathVariable(value = "numEtudiant") Integer numEtudiant) {
+        return new ResponseEntity<>(this.stageService.findStageByNumEtudiant(numEtudiant), HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<Stage>> listeAllStage() {
+        return new ResponseEntity<>(this.stageService.findAllStage(), HttpStatus.OK);
+    }
+
+    @GetMapping("/prof/{numEntreprise}")
+    public ResponseEntity<List<Stage>> listeAllStageForOneEntreprise(@PathVariable(value = "numEntreprise") Integer numEntreprise) {
+        return new ResponseEntity<>(this.stageService.findAllStageByNumEntreprise(numEntreprise), HttpStatus.OK);
+    }
+
+    @PostMapping("/creation")
+    public ResponseEntity<Stage> create(@RequestBody final Stage stage) {
+        return new ResponseEntity<>(this.stageService.create(stage), HttpStatus.OK);
+    }
+
+    @PutMapping("/modify/{numStage}")
+    public ResponseEntity<Stage> modify(@RequestParam(value = "stage") final Stage newStage,
+                                        @PathVariable(value = "numStage") Integer numStage) {
+        return new ResponseEntity<>(this.stageService.modify(newStage, numStage), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{numStage}")
+    public ResponseEntity<?> delete(@PathVariable Integer numStage) {
+        this.stageService.delete(numStage);
+        return ResponseEntity.noContent().build();
+    }
 }
--- server/src/main/java/com/ProjIR/ProjetLavalThoral/stage/StageService.java
+++ server/src/main/java/com/ProjIR/ProjetLavalThoral/stage/StageService.java
@@ -10,11 +10,47 @@ public class StageService {
     @Autowired
     private StageRepository stageRepository;
 
+    public Stage findStageByNumStage(Integer numStage) {
+        return this.stageRepository.findById(numStage).orElseThrow();
+    }
+
     public List<Stage> findAllStageByNumEntreprise(Integer numEntreprise) {
         return this.stageRepository.findAllByNumEntreprise(numEntreprise);
     }
 
+    public List<Stage> findAllStage() {
+        return this.stageRepository.findAll();
+    }
+
     public Stage findStageByNumEtudiant(Integer numEtudiant) {
         return this.stageRepository.findStageByNumEtudiant(numEtudiant);
+    }
+
+    Stage create(Stage stage) {
+        return this.stageRepository.save(stage);
+    }
+
+    Stage modify(Stage newStage, Integer id) {
+        return this.stageRepository.findById(id)
+                .map(stage -> {
+                    stage.setNumEtudiant(newStage.getNumEtudiant());
+                    stage.setNumEntreprise(newStage.getNumEntreprise());
+                    stage.setDebutStage(newStage.getDebutStage());
+                    stage.setFinStage(newStage.getFinStage());
+                    stage.setTypeStage(newStage.getTypeStage());
+                    stage.setObservation(newStage.getObservation());
+                    stage.setNumProf(newStage.getNumProf());
+                    stage.setDescProjet(newStage.getDescProjet());
+                    stage.setObservationStage(newStage.getObservationStage());
+                    return this.stageRepository.save(stage);
+                })
+                .orElseGet(() -> {
+                    newStage.setNumStage(newStage.getNumStage());
+                    return this.stageRepository.save(newStage);
+                });
+    }
+
+    public void delete(Integer id) {
+        this.stageRepository.deleteById(id);
     }
 }
